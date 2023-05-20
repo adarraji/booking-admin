840c56316e1c2dfe97f5974cc339ef896ba7c30f
adarraji <a.darraji@outlook.com>
2023-05-20 19:53:03 -0400

    pass user & hotel col dynamically in the app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/AuthContext";
+import { hotelColumns, userColumns } from "./datatablesource";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -37,7 +38,7 @@ function App() {
             <Route path="users">
               <Route index element={
                 <ProtectedRoute>
-                  <List />
+                  <List columns={userColumns} />
                 </ProtectedRoute>
               } />
               <Route path=":userId" element={
@@ -54,10 +55,10 @@ function App() {
                 }
               />
             </Route>
-            <Route path="products">
+            <Route path="hotels">
               <Route index element={
                 <ProtectedRoute>
-                  <List />
+                  <List columns={hotelColumns} />
                 </ProtectedRoute>
               } />
               <Route path=":productId" element={
