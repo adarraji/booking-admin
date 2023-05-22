5e470982959da6e7f7d9a62b2dea0515bebe5b9b
adarraji <a.darraji@outlook.com>
2023-05-22 15:50:09 -0400

    added cloudinary upload url

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -20,7 +20,7 @@ const New = ({ inputs, title }) => {
     data.append("upload_preset", "upload");
     try {
       const uploadRes = await axios.post(
-        "https://api.cloudinary.com/v1_1/lamadev/image/upload",
+        `${process.env.REACT_APP_CLOUDINARY_BASE_URL}/image/upload`,
         data
       );
 
